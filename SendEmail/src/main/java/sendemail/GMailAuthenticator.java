5d5c953ee1ae248d89df66a2ba2ffe9733dd6853
satheesh kumar p <psk30198@gmail.com>
2021-07-29 14:56:10 +0530

    Email project

---
+++ SendEmail/src/main/java/sendemail/GMailAuthenticator.java
@@ -0,0 +1,67 @@
+package sendemail;
+
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class GMailAuthenticator {
+
+
+public static void main(String[] args)throws Exception {
+	Address ad=new Address();
+	ad.sendmessage();
+	
+}
+}
+
+class Address{
+	void sendmessage()throws Exception {
+	Set<String> list=new TreeSet<>();
+	Scanner sc=new Scanner(System.in);
+	System.out.print("please enter how many mail will be sent :");
+	int a=sc.nextInt();
+	sc.nextLine();
+	for (int i=0;i<a;i++) {
+		list.add(sc.nextLine());
+	}
+	sc.close();
+	int count=0;
+	int count1=0;
+	for(String str:list) {
+		list.add(str);
+	}
+	for (String name : list)
+    {
+    	String regex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +  
+                "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+ 
+        Pattern pattern = Pattern.compile(regex);
+        String mail=name;
+      Matcher matcher = pattern.matcher(name);
+      if(matcher.matches()) {
+    	  String sendname=name.substring(0, name.indexOf('@'));
+  		
+  		System.out.println("prepare to send mail: "+sendname);
+  		
+  		Sendmail.sendme(mail,sendname);
+  		count++;
+      }
+      else {
+    	  
+    	  System.out.println("invail mail");
+    	  count1++;
+      }
+    }
+	System.out.println();
+	System.out.println("Total send message :  "+count);
+	System.out.println("Total Invail mail : "+count1);	
+	}
+}
+
+
+
+
---
+++ SendEmail/src/main/java/sendemail/Sendmail.java
@@ -0,0 +1,74 @@
+package sendemail;
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class Sendmail {
+
+	 public static void sendme(String recepient,String sendname) throws Exception {
+	       
+		 Properties properties = new Properties();
+		 
+		 properties.put("mail.smtp.auth", "true");
+		 properties.put("mail.smtp.starttls.enable", "true");
+		 properties.put("mail.smtp.host", "smtp.gmail.com");
+		 properties.put("mail.smtp.port", "587");
+		 
+	     
+	       
+	       
+	        final String myAccount = "psk30198@gmail.com";// set from mail id 
+	        final String password="**********";//set password of mail id
+	     
+
+	       
+	        
+	        Session session = Session.getInstance(properties,new Authenticator() {
+	        	@Override
+	        	protected PasswordAuthentication getPasswordAuthentication() {
+	        		
+	        		return new PasswordAuthentication(myAccount,password);
+	        	}
+	        }); 
+	     
+	        Message message =prepareMessage(session,myAccount,recepient,sendname);
+	        Transport.send(message);
+	        System.out.println("done");
+	           
+	 }
+	 public static Message prepareMessage(Session session,String myAccount,String recepient,String sendname) {
+			
+		 Message message=new MimeMessage(session);
+			try {
+				message.setFrom(new InternetAddress("myAccount"));
+				message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+				//mail of Subject useing setsubject() method
+				message.setSubject("A quick hello & congrats on the joining the GitHub Team!!");
+				//mail of body  useing Settext () method
+				message.setText("Dear "+sendname+" \n\n Greething To You !\n\n WELCOME TO MY GitHub Account.\n\nMy GitHub URL:https://github.com/satheesh30\n\n\nThank & Regards,\nMr. Satheesh kumar\nFullStack Development Trainee,\nHaaris Infotech Pvt.Ltd,\nE.mail:psk30198@gmail.com");
+				
+				return message;
+			} catch (AddressException e) {
+				e.printStackTrace();
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+			return null;
+		
+	 }
+	       
+
+	            
+	    }
+
+	
+
